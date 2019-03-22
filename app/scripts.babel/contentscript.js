dc68538f88cdca1a74c493f2766b4477f0c0dd5b
wayou <liuwayong@gmail.com>
2019-03-22 22:09:35 +0800

    fix expanding issue

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,8 +1,14 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log('rcv!')
-  Array.prototype.slice
-    .call(document.querySelectorAll(".js-details-target"))
-    .map(function(btn) {
-      btn.click();
-    });
+  setTimeout(() => {
+    Array.prototype.slice
+      .call(
+        document.querySelectorAll(
+          '.Details.js-details-container.js-news-feed-event-group:not(.open)'
+        )
+      )
+      .map(function(btn) {
+        var target = btn.querySelector('.js-details-target');
+        target && target.click();
+      });
+  }, 0);
 });
